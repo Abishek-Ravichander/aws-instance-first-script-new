94786f06f697406aa9a522559c98e60b6de464ec
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-02 18:29:34 +0530

    Update aws-instance-example.tf

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -1,17 +1,8 @@
 resource "aws_instance" "web1" {
    ami           = "ami-035b3c7efe6d061d5"
    instance_type = "t2.micro"
- }
-
-provider "aws" {
-  region  = "us-east-1"  
-  
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-
+   
+   security_groups              = [
 ingress = [
      {
        cidr_blocks      = ["10.0.0.0/16"]
@@ -39,4 +30,15 @@ ingress = [
        self             = null
      }
   ]
+
+    ]
+ }
+
+provider "aws" {
+  region  = "us-east-1"  
+  
 }
+
+
+
+
