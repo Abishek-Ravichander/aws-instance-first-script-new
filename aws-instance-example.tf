2bb8e1331b12d4eb9715ae4a2695d39c42f2302b
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-02 19:56:01 +0530

    Update aws-instance-example.tf

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web1" {
    count = 1
    ami           = "ami-035b3c7efe6d061d5"
    instance_type = "t2.micro"
-   vpc_security_group_ids = "sg-010f908f817844c6d"
+   vpc_security_group_ids = ["sg-010f908f817844c6d"]
 
  }
 
