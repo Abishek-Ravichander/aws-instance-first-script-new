d484f6079fd4b7d6e8e1c4de990edbc8c4700812
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-02 19:40:52 +0530

    sg-010f908f817844c6d

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -1,36 +1,13 @@
 resource "aws_instance" "web1" {
    count = 1
    ami           = "ami-035b3c7efe6d061d5"
-   instance_type = "t2.micro"    
+   instance_type = "t2.micro"
+   vpc_security_group_ids = "sg-010f908f817844c6d"
+
  }
 
 provider "aws" {
   region  = "us-east-1"  
   
 }
-
-resource "aws_security_group" "example" {
-  name        = "allow_tls_1"
-  description = "Allow TLS inbound traffic"
-  
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }  
-}
-
-
-
 
