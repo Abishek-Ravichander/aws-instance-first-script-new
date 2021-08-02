2a7d2a9a203aba5602c0d1a280b1379f670dde7f
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-02 18:09:49 +0530

    Update aws-instance-example.tf

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -7,3 +7,36 @@ provider "aws" {
   region  = "us-east-1"  
   
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+ingress = [
+     {
+       cidr_blocks      = ["10.0.0.0/16"]
+       from_port        = 22
+       protocol         = "tcp"
+       to_port          = 22
+       description      = "Example"
+       ipv6_cidr_blocks = null
+       prefix_list_ids  = null
+       security_groups  = null
+       self             = null
+     }
+  ]
+
+  egress = [
+     {
+       cidr_blocks      = ["10.0.0.0/16"]
+       from_port        = 0
+       protocol         = "-1"
+       to_port          = 0
+       description      = "Example"
+       ipv6_cidr_blocks = null
+       prefix_list_ids  = null
+       security_groups  = null
+       self             = null
+     }
+  ]
+}
