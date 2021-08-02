e4cc6dd2d6a20d485405df581653ea95567bf46e
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-02 18:41:31 +0530

    Update aws-instance-example.tf

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -1,37 +1,7 @@
 resource "aws_instance" "web1" {
+   count = 2
    ami           = "ami-035b3c7efe6d061d5"
-   instance_type = "t2.micro"
-   
-   security_groups              = [
-ingress = [
-     {
-       cidr_blocks      = ["10.0.0.0/16"]
-       from_port        = 22
-       protocol         = "tcp"
-       to_port          = 22
-       description      = "Example"
-       ipv6_cidr_blocks = null
-       prefix_list_ids  = null
-       security_groups  = null
-       self             = null
-     }
-  ]
-
-  egress = [
-     {
-       cidr_blocks      = ["10.0.0.0/16"]
-       from_port        = 0
-       protocol         = "-1"
-       to_port          = 0
-       description      = "Example"
-       ipv6_cidr_blocks = null
-       prefix_list_ids  = null
-       security_groups  = null
-       self             = null
-     }
-  ]
-
-    ]
+   instance_type = "t2.micro"    
  }
 
 provider "aws" {
